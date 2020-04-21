f4f10c334e199f924b81be6dff06a8d31e9920c8
Maykel <maykelmtzd@gmail.com>
2020-04-20 20:20:26 -0400

    Add examples for working with dependencies in testable code.

---
+++ TestableCodeProjTests/Dependencies/EasyToTest/PrintInvoiceCommandTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Moq;
+using Moq.AutoMock;
+using TestableCodeDemos.Module4.Shared;
+using Xunit;
+
+namespace TestableCodeDemos.Module4.Easy
+{
+    public class PrintInvoiceCommandTests
+    {
+        private PrintInvoiceCommand _command;
+        private AutoMocker _mocker;
+        private Invoice _invoice;
+
+        private const int InvoiceId = 1;
+        private const string UserName = "mrenze";
+
+        public PrintInvoiceCommandTests()
+        {
+            _invoice = new Invoice();
+
+            _mocker = new AutoMocker();
+
+            _mocker.GetMock<IDatabase>()
+                .Setup(p => p.GetInvoice(InvoiceId))
+                .Returns(_invoice);
+
+            _mocker.GetMock<IIdentityService>()
+                .Setup(p => p.GetUserName())
+                .Returns(UserName);
+
+            _command = _mocker.CreateInstance<PrintInvoiceCommand>();
+        }
+
+        [Fact]
+        public void TestExecuteShouldPrintInvoice()
+        {
+            _command.Execute(InvoiceId);
+
+            _mocker.GetMock<IInvoiceWriter>()
+                .Verify(p => p.Write(_invoice),
+                    Times.Once);
+        }
+
+        [Fact]
+        public void TestExecuteShouldLastPrintedByToCurrentUserName()
+        {
+            _command.Execute(InvoiceId);
+
+            _invoice.LastPrintedBy.Should().Equals(UserName);
+        }
+
+        [Fact]
+        public void TestExecuteShouldSaveChangesToDatabase()
+        {
+            _command.Execute(InvoiceId);
+
+            _mocker.GetMock<IDatabase>()
+                .Verify(p => p.Save(),
+                    Times.Once);
+        }
+    }
+}
